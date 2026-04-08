72aa678dda42c28b29489b84190a1fd1c5322299
Store Manager <store@manager.local>
2026-04-08 14:17:13 +0200

    Auto-sync Wed 04/08/2026 14:17:13.15

--- scripts/products.js
+++ scripts/products.js
@@ -4,5 +4,10 @@ var categories = [
         "id": 1,
         "name": "\u0627\u062c\u0647\u0632\u0647 \u0636\u063a\u0637",
         "image": "https://via.placeholder.com/150"
+    },
+    {
+        "id": 2,
+        "name": "\u0627\u062c\u0647\u0632\u0629 \u062a\u0646\u0641\u0633 \u0648\u0627\u0643\u0633\u062c\u064a\u0646",
+        "image": "https://via.placeholder.com/150"
     }
 ];
