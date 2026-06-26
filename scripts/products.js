cbbde4df98d90f4e988d446de9b3edf37a882659
Store Manager <store@manager.local>
2026-06-26 17:37:25 +0300

    Auto-sync Fri 06/26/2026 17:37:25.53

--- scripts/products.js
+++ scripts/products.js
@@ -4,5 +4,10 @@ var categories = [
         "id": 1,
         "name": "fsdfsds",
         "image": "https://via.placeholder.com/150"
+    },
+    {
+        "id": 2,
+        "name": "\u0627\u0644\u0627\u062c\u0647\u0632\u0647",
+        "image": "https://via.placeholder.com/150"
     }
 ];
